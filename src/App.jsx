547a29f29e3be853a910f6e815ebf9ad1db27823
sebifp <sebiflorinp@gmail.com>
2024-09-17 00:52:53 +0300

    ADD routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,14 @@
-import {useQuestions} from "./hooks/useQuestions.jsx";
-import Question from "./components/Question.jsx";
-import {useEffect, useState} from "react";
-import {useStages} from "./hooks/useStages.jsx";
-import ResultModal from "./components/ResultModal.jsx";
 import "./index.css"
-import Stages from "./components/Stages.jsx";
+import { Routes, Route } from "react-router-dom";
+import GamePage from "./pages/GamePage.jsx";
+import MainPage from "./pages/MainPage.jsx";
 
 function App() {
-    const [gameState, setGameState] = useState("onGoing")
-    const {currentQuestion, u5050Lifeline, getAQuestion} = useQuestions();
-    const {currentStage, nextStage, restartStage, getRewards} = useStages()
-
-
-    useEffect(() => {
-        getAQuestion("easy");
-    }, [])
-
-    if (currentQuestion === null) {
-        return <p>Loading</p>
-    }
-
-    const handleRestart = () => {
-        getAQuestion("easy")
-        setGameState("onGoing")
-        restartStage()
-    }
-
-    return(
-        <div className="grid h-screen w-screen grid-cols-3 grid-rows-3 gap-8">
-            {
-                gameState !== "onGoing" &&
-                <ResultModal handleRestart={handleRestart} reward={getRewards(gameState)}/>
-            }
-            {
-                currentStage.id > 1 &&
-                <div className="col-start-2 row-start-2 flex justify-end items-end">
-                    <button onClick={() => setGameState("withdrawn")} className="bg-gradient-to-r from-gray-800 via-gray-400 to-gray-600 p-1.5 rounded-xl">
-                        <div className="text-xl px-14 py-2.5 bg-gradient-to-r from-slate-900 via-blue-900 to-slate-900 rounded-xl text-white">Withdraw</div>
-                    </button>
-                </div>
-            }
-            <Question
-                question={currentQuestion}
-                nextQuestion={getAQuestion}
-                setGameState={setGameState}
-                currentStage={currentStage}
-                nextStage={nextStage}
-            />
-            <Stages currentStage={currentStage} lifeline5050={u5050Lifeline}/>
-        </div>
+    return (
+        <Routes>
+            <Route path="/" element={<MainPage />}/>
+            <Route path="/game" element={<GamePage />}/>
+        </Routes>
     )
 }
 
---
+++ src/pages/GamePage.jsx
@@ -0,0 +1,54 @@
+import {useEffect, useState} from "react";
+import {useQuestions} from "../hooks/useQuestions.jsx";
+import {useStages} from "../hooks/useStages.jsx";
+import ResultModal from "../components/ResultModal.jsx";
+import Question from "../components/Question.jsx";
+import Stages from "../components/Stages.jsx";
+
+function GamePage() {
+    const [gameState, setGameState] = useState("onGoing")
+    const {currentQuestion, u5050Lifeline, getAQuestion} = useQuestions();
+    const {currentStage, nextStage, restartStage, getRewards} = useStages()
+
+
+    useEffect(() => {
+        getAQuestion("easy");
+    }, [])
+
+    if (currentQuestion === null) {
+        return <p>Loading</p>
+    }
+
+    const handleRestart = () => {
+        getAQuestion("easy")
+        setGameState("onGoing")
+        restartStage()
+    }
+
+    return(
+        <div className="grid h-screen w-screen grid-cols-3 grid-rows-3 gap-8">
+            {
+                gameState !== "onGoing" &&
+                <ResultModal handleRestart={handleRestart} reward={getRewards(gameState)}/>
+            }
+            {
+                currentStage.id > 1 &&
+                <div className="col-start-2 row-start-2 flex justify-end items-end">
+                    <button onClick={() => setGameState("withdrawn")} className="bg-gradient-to-r from-gray-800 via-gray-400 to-gray-600 p-1.5 rounded-xl">
+                        <div className="text-xl px-14 py-2.5 bg-gradient-to-r from-slate-900 via-blue-900 to-slate-900 rounded-xl text-white">Withdraw</div>
+                    </button>
+                </div>
+            }
+            <Question
+                question={currentQuestion}
+                nextQuestion={getAQuestion}
+                setGameState={setGameState}
+                currentStage={currentStage}
+                nextStage={nextStage}
+            />
+            <Stages currentStage={currentStage} lifeline5050={u5050Lifeline}/>
+        </div>
+    )
+}
+
+export default GamePage
