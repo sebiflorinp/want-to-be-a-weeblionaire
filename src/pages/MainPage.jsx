5535b648dd44460155aaa260c91d6a2d880606c7
sebiflorinp <sebiflorinp@gmail.com>
2024-09-26 13:01:38 +0300

    Fix another page bg

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 function MainPage() {
   return (
-    <div className="flex h-screen w-screen justify-start items-end pl-96 pb-14 bg-title-image bg-cover bg-no-repeat bg-center">
+    <div className="flex h-screen w-screen justify-start items-end pl-64 2xl:pl-96 pb-3.5 2xl:pb-14 bg-title-image bg-cover bg-no-repeat bg-center">
       <Link
         to="/game"
         className="rounded-xl bg-gradient-to-r from-gray-800 via-gray-400 to-gray-600 p-1.5"
       >
-        <div className="bg-gradient-to-r from-slate-900 via-blue-900 to-slate-900 rounded-xl px-20 py-4">
+        <div className="bg-gradient-to-r from-slate-900 via-blue-900 to-slate-900 rounded-xl  px-16 2xl:px-20 py-3 2xl:py-4">
           <p className="font-bold text-3xl text-white">Play</p>
         </div>
       </Link>
